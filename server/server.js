924026e1d839b56370817806f35a339abd1c5824
Akshay Neje <akshayneje2823@gmail.com>
2024-03-13 17:34:39 +0530

    added deplyoment details

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 import authRouter from "./routes/auth.route.js";
 import messageRoute from './routes/message.route.js'
@@ -12,6 +13,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
+const __dirname = path.resolve()
+
 // middleware
 app.use(express.json());
 app.use(cookieParser()); // cookie reader
@@ -19,6 +22,13 @@ app.use(cookieParser()); // cookie reader
 app.use("/api/auth", authRouter);
 app.use('/api/message',messageRoute);
 app.use('/api/users',userRoute);
+
+// deployment
+app.use(express.static(path.join(__dirname,'/client/build')))
+
+app.get('*',(req,res) =>{
+  res.sendFile(path.join(__dirname,"client","build","index.html"))
+})
 
 
 // socket server
